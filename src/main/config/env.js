3b4d8f17f22039f38018f2408b8ce346d12b8fbe
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-10-08 12:26:25 -0300

    move loginRouter composition to its own file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/twintto-api'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/twintto-api',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
