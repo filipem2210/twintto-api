dc239d080623ff89e5cff957dc18bb8b029d77ce
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-10-08 12:49:37 -0300

    improve LoginRouterComposer

--- src/main/composers/login-router-composer.js
+++ src/main/composers/login-router-composer.js
@@ -1,4 +1,3 @@
-const LoginRouter = require('../../presentation/routers/login-router')
 const AuthUseCase = require('../../domain/usecases/auth-usecase')
 const EmailValidator = require('../../utils/helpers/email-validator')
 const LoadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository')
@@ -7,20 +6,22 @@ const Encrypter = require('../../utils/helpers/encrypter')
 const TokenGenerator = require('../../utils/helpers/token-generator')
 const { tokenSecret } = require('../config/env')
 
-const tokenGenerator = new TokenGenerator(tokenSecret)
-const encrypter = new Encrypter()
-const updateAccessTokenRepository = new UpdateAccessTokenRepository()
-const loadUserByEmailRepository = new LoadUserByEmailRepository()
-const emailValidator = new EmailValidator()
-const authUseCase = new AuthUseCase({
-  loadUserByEmailRepository,
-  updateAccessTokenRepository,
-  encrypter,
-  tokenGenerator
-})
-const loginRouter = new LoginRouter({
-  authUseCase,
-  emailValidator
-})
-
-module.exports = loginRouter
+module.exports = class LoginRouter {
+  static compose () {
+    const tokenGenerator = new TokenGenerator(tokenSecret)
+    const encrypter = new Encrypter()
+    const updateAccessTokenRepository = new UpdateAccessTokenRepository()
+    const loadUserByEmailRepository = new LoadUserByEmailRepository()
+    const emailValidator = new EmailValidator()
+    const authUseCase = new AuthUseCase({
+      loadUserByEmailRepository,
+      updateAccessTokenRepository,
+      encrypter,
+      tokenGenerator
+    })
+    return new LoginRouter({
+      authUseCase,
+      emailValidator
+    })
+  }
+}
