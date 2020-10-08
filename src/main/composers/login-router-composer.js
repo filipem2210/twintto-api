fe58b065c467bb03d4e76c9c437640fbc5c08140
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-10-08 14:25:34 -0300

    add login route integration to ensure 200 response when valid credentials are provided

--- src/main/composers/login-router-composer.js
+++ src/main/composers/login-router-composer.js
@@ -1,3 +1,4 @@
+const LoginRouter = require('../../presentation/routers/login-router')
 const AuthUseCase = require('../../domain/usecases/auth-usecase')
 const EmailValidator = require('../../utils/helpers/email-validator')
 const LoadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository')
@@ -6,7 +7,7 @@ const Encrypter = require('../../utils/helpers/encrypter')
 const TokenGenerator = require('../../utils/helpers/token-generator')
 const { tokenSecret } = require('../config/env')
 
-module.exports = class LoginRouter {
+module.exports = class LoginRouterComposer {
   static compose () {
     const tokenGenerator = new TokenGenerator(tokenSecret)
     const encrypter = new Encrypter()
