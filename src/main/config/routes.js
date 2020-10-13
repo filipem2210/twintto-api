a9eae9b1817fdc674df337f986968ceeb88ef966
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-10-13 15:51:09 -0300

    remove route not found rule

--- src/main/config/routes.js
+++ src/main/config/routes.js
@@ -4,5 +4,4 @@ const fg = require('fast-glob')
 module.exports = app => {
   app.use('/api', router)
   fg.sync('**/src/main/routes/**routes.js').forEach(file => require(`../../../${file}`)(router))
-  app.use((req, res) => { res.status(404).json({ error: 'Route not found' }) })
 }
