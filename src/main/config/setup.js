3370f1a03beda55f94fda64f8b75e8bec88589fa
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-10-13 18:48:11 -0300

    change express setup order

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,5 +1,5 @@
-const helmet = require('../middlewares/helmet')
 const cors = require('../middlewares/cors')
+const helmet = require('../middlewares/helmet')
 const jsonParser = require('../middlewares/json-parser')
 const contentType = require('../middlewares/content-type')
 const compression = require('../middlewares/compression')
@@ -8,8 +8,8 @@ const morgan = require('../loggers/morgan')
 
 module.exports = app => {
   app.disable('x-powered-by')
-  app.use(helmet)
   app.use(cors)
+  app.use(helmet)
   app.use(jsonParser)
   app.use(contentType)
   app.use(compression)
