b73b04a1913a806de0b7ffcf2d3eafa9353cf17d
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-10-13 15:06:03 -0300

    fix return type

--- src/main/config/routes.js
+++ src/main/config/routes.js
@@ -4,5 +4,5 @@ const fg = require('fast-glob')
 module.exports = app => {
   app.use('/api', router)
   fg.sync('**/src/main/routes/**routes.js').forEach(file => require(`../../../${file}`)(router))
-  app.use((req, res) => { res.status(404).send({ error: 'Route not found' }) })
+  app.use((req, res) => { res.status(404).json({ error: 'Route not found' }) })
 }
