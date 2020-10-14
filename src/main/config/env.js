088cf85f0d0e8a46d8078ef5265a95dcdb0e4bf3
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-10-13 21:03:30 -0300

    update environment variables

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://twintto_db:27017/twintto',
-  tokenSecret: process.env.TOKEN_SECRET || 'secret',
-  port: process.env.PORT || 5858
+  tokenSecret: process.env.SECRET_KEY || 'Tvv1ntT0',
+  port: process.env.APP_PORT || 5858
 }
