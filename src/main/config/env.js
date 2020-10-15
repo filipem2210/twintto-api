6890dce52079131b7cbc32210b7b77d2239015c9
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-10-15 14:16:13 -0300

    fix mongo connection string

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://twintto_db:27017/twintto',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://twintto:twintto@twintto_db:27017/twintto?authSource=admin',
   tokenSecret: process.env.SECRET_KEY || 'Tvv1ntT0',
   port: process.env.APP_PORT || 5858
 }
