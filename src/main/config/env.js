e7499f56ebe952e9214c34621f025e69efe99b1d
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-10-19 21:55:51 -0300

    add redis host and redis port to env file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -6,8 +6,8 @@ module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://twintto:twintto@twintto_db:27017/twintto?authSource=admin',
   tokenSecret: process.env.SECRET_KEY || 'Tvv1ntT0',
   port: process.env.APP_PORT || 5858,
-  sentryDsn: process.env.SENTRY_DSN,
-  redisHost: process.env.REDIS_HOST,
+  redisHost: process.env.REDIS_HOST || '127.0.0.1',
   redisPort: process.env.REDIS_PORT || 6379,
+  sentryDsn: process.env.SENTRY_DSN,
   privateKey
 }
