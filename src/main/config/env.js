f090f7ca9660d022aa63f74179832eb770defebf
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-10-20 18:14:32 -0300

    update REDIS_HOST value

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -6,7 +6,7 @@ module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://twintto:twintto@twintto_db:27017/twintto?authSource=admin',
   tokenSecret: process.env.SECRET_KEY || 'Tvv1ntT0',
   port: process.env.APP_PORT || 5858,
-  redisHost: process.env.REDIS_HOST || '127.0.0.1',
+  redisHost: process.env.REDIS_HOST || '0.0.0.0',
   redisPort: process.env.REDIS_PORT || 6379,
   sentryDsn: process.env.SENTRY_DSN,
   privateKey
